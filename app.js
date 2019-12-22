5759c2206ad5b88cd9f1b5f0528737148a5417a4
cryptorootz <cryptorootz@protonmail.com>
2019-12-22 12:41:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://rootzman:danceflow1@cluster0-bsgyj.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+    { useUnifiedTopology: true },
+     () => {console.log('connected to DB')
+});
+
+// Listening to Server
+app.listen(3000);
